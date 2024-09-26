51eba46dec06765e1cbf8fbc6f31bd9057e53256
暖阳 <15255096386@163.com>
2024-09-26 10:32:29 +0800

    feat(monitor): add map component in monitor view

---
+++ template-vue3-ts-base/src/views/monitor/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 地图组件 -->
+  <MvMap @map-load="onMapLoad" />
+</template>
+
+<script setup lang="ts">
+import MvMap from '@/components/business/mv-map/index.vue';
+const map = ref<AMap.Map>(); // 地图实例
+/**
+ * @function
+ * @todo 地图加载完毕回调
+ * @param {} data 地图实例
+ **/
+function onMapLoad(data: AMap.Map) {
+  map.value = data;
+  console.log('地图加载完毕', data);
+  console.log('地图中心点', map.value.getCenter());
+}
+</script>
+
+<style scoped lang="less"></style>
