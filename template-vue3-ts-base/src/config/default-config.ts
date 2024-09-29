107c8fd03c7cc6b37c8232225948adcf2486cfbd
暖阳 <15255096386@163.com>
2024-09-29 16:07:29 +0800

    feat: Remove unnecessary comments and copyright statements

--- template-vue3-ts-base/src/config/default-config.ts
+++ template-vue3-ts-base/src/config/default-config.ts
@@ -1,11 +1,3 @@
-/*
- * @Description:系统配置文件
- * @Author: smigoo(xsmigoo@gmail.com)
- * @Date: 2024-04-27 20:11:02
- * @LastEditors: wangyang
- * @LastEditTime: 2024-09-24 09:24:43
- * @Copyright: © 2024 Microvideo
- */
 export const network = {
   /**
    * 操作正常code，支持String、Array、int多种类型
