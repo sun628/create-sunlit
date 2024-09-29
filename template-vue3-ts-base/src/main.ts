22caf3c228edc362c070d8c3170173515304fe5c
暖阳 <15255096386@163.com>
2024-09-29 16:06:12 +0800

    feat: rearrange imports in main.ts files

--- template-vue3-ts-base/src/main.ts
+++ template-vue3-ts-base/src/main.ts
@@ -1,15 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from '@/routers';
-import pinia from '@/store/index';
-import GlobalComp from '@/components/index';
 import 'virtual:uno.css'; // css reset
 import 'ant-design-vue/dist/reset.css';
 import '@/assets/styles/index.less';
 import '@/utils/browser';
+import router from '@/routers';
+import pinia from '@/store/index';
 import directives from '@/directives/index'; // custom directives
 
 const app = createApp(App);
-app.use(GlobalComp); //批量注册全局组件
 
 app.use(router).use(pinia).use(directives).mount('#app');
