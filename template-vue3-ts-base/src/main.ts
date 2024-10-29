c07665ea24778ffed20c4f7c72cdbc71a9602dc8
暖阳 <15255096386@163.com>
2024-10-29 10:35:17 +0800

    feat: Update router import and refactor app instantiation

--- template-vue3-ts-base/src/main.ts
+++ template-vue3-ts-base/src/main.ts
@@ -4,10 +4,14 @@ import 'virtual:uno.css'; // css reset
 import 'ant-design-vue/dist/reset.css';
 import '@/assets/styles/index.less';
 import '@/utils/browser';
-import router from '@/routers';
+import router from '@/router';
 import pinia from '@/store/index';
 import directives from '@/directives/index'; // custom directives
+import SvgIcon from '~virtual/svg-component';
 
 const app = createApp(App);
 
-app.use(router).use(pinia).use(directives).mount('#app');
+app.component(SvgIcon.name!, SvgIcon);
+
+app.use(router).use(pinia).use(directives);
+app.mount('#app');
