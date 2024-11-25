5c7c4d9c0833c51f46d103db3b08f952bcd5202d
暖阳 <15255096386@163.com>
2024-11-25 13:18:21 +0800

    refactor(Submenu): optimize submenu rendering logic

--- template-vue3-ts-base/src/layouts/components/menu/components/SubMenu.vue
+++ template-vue3-ts-base/src/layouts/components/menu/components/SubMenu.vue
@@ -1,15 +1,17 @@
 <template>
-  <ASubMenu v-if="item.children && item.children.length > 1" :key="item.name">
+  <ASubMenu :key="item.name">
     <template #icon>
       <SvgIcon v-if="item.meta?.icon" :name="item.meta.icon" />
     </template>
     <template #title>{{ item.meta?.title }}</template>
     <template v-for="subItem in item.children" :key="subItem.name">
-      <SubMenu v-if="subItem.children && subItem.children.length > 0" :item="subItem" />
+      <SubMenu
+        v-if="subItem.children && (subItem.children.length > 1 || subItem?.meta?.alwaysShow)"
+        :item="subItem"
+      />
       <MenuItem v-else :item="subItem" />
     </template>
   </ASubMenu>
-  <MenuItem v-else :item="item" />
 </template>
 <script setup lang="ts">
 import { RouteRecordRaw } from 'vue-router';
