6bb5ce8054cf7939591820b853ab924034a593fb
暖阳 <15255096386@163.com>
2025-04-10 10:26:27 +0800

    refactor: 移除未使用的 SvgIcon 组件并优化 main.ts 文件结构

--- template-vue3-ts-base/src/main.ts
+++ template-vue3-ts-base/src/main.ts
@@ -6,13 +6,10 @@ import '@/utils/rem';
 import router from '@/router';
 import pinia from '@/store/index';
 import directives from '@/directives/index'; // custom directives
-import SvgIcon from '~virtual/svg-component';
 import VChart from 'vue-echarts';
 
 const app = createApp(App);
 
-app.component(SvgIcon.name!, SvgIcon);
 app.component('VChart', VChart);
-
 app.use(router).use(pinia).use(directives);
 app.mount('#app');
