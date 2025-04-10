273da169c643f685547406e174e341c72d9ad53f
暖阳 <15255096386@163.com>
2025-04-10 10:26:15 +0800

    feat: 在 App.vue 中添加 StyleProvider 以支持 px2rem 转换
    - 引入 StyleProvider 和 px2remTransformer
    - 将 AConfigProvider 包裹在 StyleProvider 中以实现样式转换

--- template-vue3-ts-base/src/App.vue
+++ template-vue3-ts-base/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <AConfigProvider :locale="zhCN" :theme="antTheme">
-    <router-view></router-view>
-  </AConfigProvider>
+  <StyleProvider :transformers="[px2rem]">
+    <AConfigProvider :locale="zhCN" :theme="antTheme">
+      <router-view></router-view>
+    </AConfigProvider>
+  </StyleProvider>
 </template>
 <script setup lang="ts">
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import { antTheme } from '@/config';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
+import { px2remTransformer, StyleProvider } from 'ant-design-vue';
+
+const px2rem = px2remTransformer({
+  rootValue: $config.pxtorem.rootValue,
+});
+
 dayjs.locale('zh-cn');
 </script>
