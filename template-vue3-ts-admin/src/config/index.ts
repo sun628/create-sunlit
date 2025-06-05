479977769f9c9b8f9717c368b4601ef4cea2e3ff
王杨 <wangyang@microvideo.cn>
2025-06-05 09:45:10 +0800

    feat: 添加 layoutSetting 默认配置项

--- template-vue3-ts-admin/src/config/index.ts
+++ template-vue3-ts-admin/src/config/index.ts
@@ -6,6 +6,13 @@ export * from './types';
 
 const env = loadEnv();
 
+/**
+ * layoutSetting 项目默认配置项
+ * @param primaryColor - 默认主题色, 如果修改颜色不生效，请清理 localStorage
+ * @param navTheme - sidebar theme ['dark', 'light'] 两种主题
+ * @param layout - 整体布局方式 ['vertical', 'horizontal'] 两种布局
+ * @param headerHeight - 头部高度
+ */
 export const layoutSetting: LayoutSetting = {
   layout: 'vertical', // 整体布局方式
   navTheme: 'dark', // sidebar主题
@@ -17,7 +24,7 @@ export const layoutSetting: LayoutSetting = {
 export const serversConfig = [
   {
     name: 'base_server',
-    url: env.VITE_BASE_API as string
+    url: env.VITE_BASE_API_URL
   },
   {
     name: 'flow_server',
