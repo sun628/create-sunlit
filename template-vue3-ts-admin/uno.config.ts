2f53a0212672363dca479886916f0fe26635e66b
王杨 <wangyang@microvideo.cn>
2025-06-06 17:12:01 +0800

    feat: 简化代码

--- template-vue3-ts-admin/uno.config.ts
+++ template-vue3-ts-admin/uno.config.ts
@@ -48,7 +48,6 @@ export default defineConfig({
         'ant-design': () => import('@iconify-json/ant-design/icons.json').then((i) => i.default),
         svg: FileSystemIconLoader(iconsDir, (svg) => {
           // 如果 SVG 文件未定义 `fill` 属性，则默认填充 `currentColor`
-          // 这样图标颜色会继承文本颜色，方便在不同场景下适配
           return svg.includes('fill="') ? svg : svg.replace(/^<svg /, '<svg fill="currentColor" ');
         })
       }
