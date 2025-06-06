d27db86149b73db978970619bfcc5a2ed0c82aba
王杨 <wangyang@microvideo.cn>
2025-06-06 17:10:22 +0800

    feat: 更新类型定义

--- template-vue3-ts-admin/src/typings/global.d.ts
+++ template-vue3-ts-admin/src/typings/global.d.ts
@@ -9,6 +9,19 @@ declare global {
 declare module 'vue-router' {
   interface RouteMeta extends VRouteMeta {
     title: string;
+    icon?: string;
+    /** 不在菜单中显示 */
+    hideInMenu?: boolean;
+     /** 在菜单中隐藏子节点 */
+    hideChildrenInMenu?: boolean;
+    loaded?: boolean;
+    /** 是否外链 */
+    isExt?: boolean;
+    /** 外链打开方式
+     * 1: 新窗口打开
+     * 2: 内嵌 iframe
+     */
+    extOpenMode?: 1 | 2;
   }
 }
 
