c03ccb3144d0ba02d9015b283337a7fa533f8387
nuanyang <15255096386@163.com>
2025-07-21 14:07:47 +0800

    feat: 更新 Prettier 配置，优化代码格式化规则，新增括号间距和箭头函数参数括号设置

--- template/vue3-ts-base/.prettierrc.cjs
+++ template/vue3-ts-base/.prettierrc.cjs
@@ -1,14 +1,13 @@
 module.exports = {
   printWidth: 100, // 一行最多多少个字符
-  singleQuote: true, // 使用制表符而不是空格缩进行
-  useTabs: false, // 使用制表符而不是空格缩进行
-  jsxSingleQuote: false, // 在JSX中使用单引号而不是双引号
-  semi: true, // 在语句末尾是否需要分号
-  trailingComma: 'none', // 在对象或数组最后一个元素后面是否加逗号
-  //在 windows 操作系统中换行符通常是回车 (CR) 加换行分隔符 (LF)，也就是回车换行(CRLF)，
-  //然而在 Linux 和 Unix 中只使用简单的换行分隔符 (LF)。
-  //对应的控制字符为 "\n" (LF) 和 "\r\n"(CRLF)。auto意为保持现有的行尾
-  // 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
-  endOfLine: 'auto',
-  htmlWhitespaceSensitivity: 'ignore' // 指定HTML文件的全局空格敏感度 css\strict\ignore
+  singleQuote: true, // 使用单引号
+  useTabs: false, // 不使用制表符缩进
+  jsxSingleQuote: false, // 在JSX中使用双引号
+  semi: true, // 在语句末尾添加分号
+  trailingComma: 'none', // 不在对象或数组最后一个元素后面加逗号
+  endOfLine: 'auto', // 自动检测行尾
+  htmlWhitespaceSensitivity: 'ignore', // 指定HTML文件的全局空格敏感度
+  bracketSpacing: true, // 在对象字面量中的括号之间添加空格
+  arrowParens: 'always', // 箭头函数参数总是使用括号
+  bracketSameLine: false // 多行HTML元素的>放在下一行
 };
