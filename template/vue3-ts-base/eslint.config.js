b2f5b405199e24ca511594295dd498911d1229b1
nuanyang <15255096386@163.com>
2025-07-21 13:54:01 +0800

    feat: 更新 ESLint 配置，新增全局变量和解析器设置，优化 TypeScript 支持

--- template/vue3-ts-base/eslint.config.js
+++ template/vue3-ts-base/eslint.config.js
@@ -1,16 +1,27 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tsEslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 import parserVue from 'vue-eslint-parser';
+import { configs as tsConfigs, parser as tsParser } from 'typescript-eslint';
+import eslintPluginImportX, { flatConfigs as importXFlatConfigs } from 'eslint-plugin-import-x';
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
 import importJson from './.eslintrc-auto-import.json' with { type: 'json' };
 import globalJson from './.eslint.globals.json' with { type: 'json' };
+const typeScriptExtensions = ['.ts', '.tsx', '.cts', '.mts'];
 
+const allExtensions = [...typeScriptExtensions, '.js', '.jsx', '.cjs', '.mjs'];
 export default [
   pluginJs.configs.recommended,
-  ...tsEslint.configs.recommended,
+  importXFlatConfigs.recommended,
+  importXFlatConfigs.typescript,
+  ...tsConfigs.recommended,
   ...pluginVue.configs['flat/essential'],
+  {
+    plugins: {
+      import: eslintPluginImportX
+    }
+  },
   pluginPrettierRecommended,
   { files: ['**/*.{js,mjs,cjs,jsx,ts,tsx,vue}'] },
   {
@@ -29,18 +40,41 @@ export default [
       parser: parserVue,
       parserOptions: {
         ecmaFeatures: { jsx: true },
-        parser: tsEslint.parser
+        parser: tsParser
+      }
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+        'vue-eslint-parser': ['.vue']
+      },
+      'import-x/extensions': allExtensions,
+      'import-x/external-module-folders': ['node_modules', 'node_modules/@types'],
+      'import-x/parsers': {
+        '@typescript-eslint/parser': [...typeScriptExtensions]
+      },
+      'import-x/resolver': {
+        alias: {
+          map: [
+            ['@', './src'],
+            ['@base-components', './src/components/base-components'],
+            ['@business-components', './src/components/business-components']
+          ],
+          extensions: allExtensions
+        },
+        typescript: true
       }
     },
     rules: {
       'no-debugger': 'off',
-      'no-undefined': 'error',
       'no-restricted-syntax': 'error',
       'no-global-assign': 'error',
       'comma-dangle': 'off',
+      'no-with': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-expressions': ['off'],
       '@typescript-eslint/no-namespace': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'off', // 允许非空断言运算符(!)
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
@@ -49,11 +83,24 @@ export default [
           caughtErrorsIgnorePattern: '^_'
         }
       ],
-      'prefer-const': [
+      '@typescript-eslint/ban-ts-comment': [
         'error',
         {
-          destructuring: 'any',
-          ignoreReadBeforeAssign: false
+          'ts-ignore': false,
+          'ts-expect-error': 'allow-with-description'
+        }
+      ],
+      'prefer-const': [
+        'warn',
+        {
+          destructuring: 'all', // 仅当所有变量都可以const化时才提示
+          ignoreReadBeforeAssign: true // 忽略在声明前被读取的变量
+        }
+      ],
+      'import-x/no-unresolved': [
+        'error',
+        {
+          ignore: ['^virtual:.*', 'virtual:uno.css']
         }
       ]
     }
@@ -61,28 +108,16 @@ export default [
   {
     files: ['**/*.vue'],
     languageOptions: {
-      parserOptions: { ecmaFeatures: { jsx: true }, parser: tsEslint.parser }
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        parser: tsParser,
+        extraFileExtensions: ['.vue'],
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
     },
     rules: {
       'vue/multi-word-component-names': 'off',
-      'vue/attributes-order': [
-        'warn',
-        {
-          order: [
-            'DEFINITION',
-            'LIST_RENDERING',
-            'CONDITIONALS',
-            'RENDER_MODIFIERS',
-            'GLOBAL',
-            'UNIQUE',
-            'TWO_WAY_BINDING',
-            'OTHER_DIRECTIVES',
-            'OTHER_ATTR',
-            'EVENTS',
-            'CONTENT'
-          ]
-        }
-      ],
       'vue/component-name-in-template-casing': [
         'error',
         'PascalCase',
