50965851e0d2892b38d687229eb395df12be4a17
nuanyang <15255096386@163.com>
2025-07-21 14:52:51 +0800

    feat: 优化 useReset，支持部分重置功能

--- template/vue3-ts-base/src/hooks/useReset.ts
+++ template/vue3-ts-base/src/hooks/useReset.ts
@@ -1,40 +1,63 @@
-import { reactive, ref, Ref } from 'vue';
+import { reactive, ref } from 'vue';
 import { cloneDeep } from 'lodash-es';
 /**
  * @description  重置表单数据
  * @example
  * const [formState, resetForm] = useResetRef({a:1})
  * resetForm()
+ * const [formState, resetForm] = useResetRef({a:1})
+ * resetForm(['a'])
+ *
+ * 重置整个表单reactive
  * const [formState, resetForm] = useResetReactive({a:1})
  * resetForm()
+ * const [formState, resetForm] = useResetRef({a:1,b:2})
+ * resetForm(['a','b'])
  */
 
 /**
  * @function
- * @todo 重置数据
+ * @description 重置数据
  * @param value 表单数据
  **/
-export function useResetRef<T>(value: T) {
+export function useResetRef<T extends Recordable>(value: T) {
   const initialValue = cloneDeep(value);
   const state = ref(value) as Ref<T>;
 
-  const reset = () => {
-    state.value = cloneDeep(initialValue);
+  const reset = (keys?: Array<keyof T>) => {
+    if (keys) {
+      keys.forEach((key) => {
+        if (key in initialValue) {
+          state.value[key] = initialValue[key];
+        }
+      });
+    } else {
+      state.value = cloneDeep(initialValue);
+    }
   };
   return [state, reset] as const;
 }
 
 /**
  * @function
- * @todo reactive重置表单数据
+ * @description reactive重置表单数据
  * @param {Object} value - 表单数据
  **/
-export function useResetReactive<T extends Record<string, any>>(value: T) {
+export function useResetReactive<T extends Recordable>(value: T) {
   const initialValue = cloneDeep(value);
   const state = reactive(value);
-  const reset = () => {
-    Object.keys(state).forEach((key) => delete state[key]);
-    Object.assign(state, cloneDeep(initialValue));
+
+  const reset = (keys?: Array<keyof T>) => {
+    if (keys) {
+      keys.forEach((key) => {
+        if (key in initialValue) {
+          (state as any)[key] = initialValue[key];
+        }
+      });
+    } else {
+      Object.keys(state).forEach((key) => delete (state as any)[key]);
+      Object.assign(state, initialValue);
+    }
   };
   return [state, reset] as const;
 }
